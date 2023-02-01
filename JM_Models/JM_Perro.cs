31fefc9d455871263a1538daa113ae500f8d26d2
Jose Merlo <josemielmer2002@hotmail.com>
2023-02-01 08:52:37 -0500

    Perro model completado

--- JM_Models/JM_Perro.cs
+++ JM_Models/JM_Perro.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MaxLengthAttribute = SQLite.MaxLengthAttribute;
 
 namespace JM_ExamenProgreso3.JM_Models
 {
-    internal class JM_Perro
+    [Table("Perro")]
+    public class JM_Perro
     {
+        [PrimaryKey, AutoIncrement]
+        public int JM_id { get; set; }
+
+        [MaxLength(10)]
+        public string JM_nombre{ get; set; }
+        public string JM_raza { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime CreationDate { get; set; }
     }
 }
