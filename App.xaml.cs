de3ec74f68a1d9035bcdfb742cceac330dbbf037
Jose Merlo <josemielmer2002@hotmail.com>
2023-02-01 09:52:50 -0500

    Api clase creada

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,16 @@
-﻿namespace JM_ExamenProgreso3;
+﻿using JM_ExamenProgreso3.JM_Data;
+
+namespace JM_ExamenProgreso3;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public static JM_ExamenDatabase ExamenRepo { get; set; }
+    public App(JM_ExamenDatabase repo)
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new AppShell();
+
+        ExamenRepo = repo;
+    }
 }
--- JM_Api/JM_ClaseApi.cs
+++ JM_Api/JM_ClaseApi.cs
@@ -8,6 +8,13 @@ namespace JM_ExamenProgreso3.JM_Api
 {
     internal class JM_ClaseApi
     {
-
+        public class Root
+        {
+            public int id { get; set; }
+            public string breed { get; set; }
+            public string origin { get; set; }
+            public string url { get; set; }
+            public string img { get; set; }
+        }
     }
 }
