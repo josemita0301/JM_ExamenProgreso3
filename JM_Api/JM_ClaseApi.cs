31ffe250780c59ad07e82c56a63a904219451e43
Jose Merlo <josemielmer2002@hotmail.com>
2023-02-01 08:35:19 -0500

    Creación estructura del proyecto

---
+++ JM_Api/JM_ClaseApi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JM_ExamenProgreso3.JM_Api
+{
+    internal class JM_ClaseApi
+    {
+    }
+}
